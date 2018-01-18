34c270af2813d301ee0e7b509e24e7be0bbb4632
kikuchi-makoto <kikuchi-makoto5@aeonpeople.biz>
2018-01-18 22:01:06 +0900

    webpack.config.jsの書き方修正。cssとjsディレクトリ作成

--- webpack.config.js
+++ webpack.config.js
@@ -1,58 +1,67 @@
 const webpack = require('webpack');
 const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const extractSass = new ExtractTextPlugin('css/[name].css');
 
-const extractSass = new ExtractTextPlugin({
-  filename: './style.css',
-  disable: process.env.NODE_ENV === 'development',
-});
-
-module.exports = {
-  entry: [
-    './client/src/js/app.js',
-    './client/src/sass/style.scss',
-  ],
-  output: {
-    path: path.resolve('./dist'),
-    filename: 'app.js',
-  },
-  devServer: {
-    contentBase: './dist',
-    watchContentBase: true,
-    port: 3100,
-    open: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js[x]?$/,
-        use: [
-          {
-            loader: 'babel-loader',
-            options: {
-              presets: ['react', 'es2015'],
-            },
+module.exports = [
+  {
+    entry: {
+      app: ['./client/src/js/app.js'],
+    },
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: '[name].js',
+    },
+    devServer: {
+      contentBase: './dist',
+      watchContentBase: true,
+      port: 3000,
+      open: true,
+    },
+    module: {
+      loaders: [
+        {
+          test: /\.js[x]?$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader',
+          query: {
+            presets: ['react', 'es2015'],
           },
-        ],
-      },
-      {
-        test: /\.scss$/,
-        use: extractSass.extract({
-          use: [{
-            loader: 'css-loader',
-          }, {
-            loader: 'sass-loader?outputStyle=compressed',
-          }],
-          fallback: 'style-loader',
-        }),
-      },
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.js', '.jsx'],
+    },
+    // plugins: [
+    //   new webpack.optimize.UglifyJsPlugin(),
+    // ],
+  },
+  {
+    entry: {
+      style: ['./client/src/sass/style.scss'],
+    },
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'css/[name].css',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.scss$/,
+          use: extractSass.extract({
+            use: [{
+              loader: 'css-loader',
+            }, {
+              loader: 'sass-loader',
+            }],
+            fallback: 'style-loader',
+          }),
+        },
+      ],
+    },
+    plugins: [
+      extractSass,
     ],
   },
-  resolve: {
-    extensions: ['.js', '.jsx'],
-  },
-  plugins: [
-    extractSass,
-    new webpack.optimize.UglifyJsPlugin(),
-  ],
-};
+];
