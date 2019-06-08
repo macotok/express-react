63bb8c034be831dc219dc750ab40b8a4cad90764
macotok <monochromer@r8.ucom.ne.jp>
2019-06-09 00:07:58 +0900

    update all package for webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,75 +1,64 @@
-const webpack = require('webpack');
 const path = require('path');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const extractSass = new ExtractTextPlugin('css/[name].css');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = [
-  {
-    entry: {
-      app: ['./client/src/js/app.js'],
-    },
-    output: {
-      path: path.resolve(__dirname, 'dist'),
-      filename: 'js/[name].js',
-    },
-    devServer: {
-      contentBase: './dist',
-      watchContentBase: true,
-      port: 3000,
-      open: true,
-    },
-    module: {
-      loaders: [
-        {
-          test: /\.js[x]?$/,
-          exclude: /node_modules/,
-          loader: 'babel-loader',
-          query: {
-            presets: ['react', 'env'],
+module.exports = {
+  entry: {
+    app: [
+      './client/src/js/app.js',
+      './client/src/sass/style.scss',
+    ],
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/[name].js',
+  },
+  devServer: {
+    contentBase: './dist',
+    watchContentBase: true,
+    port: 3000,
+    open: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js[x]?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
           },
-        },
-      ],
-    },
-    devtool: 'eval-source-map',
-    resolve: {
-      extensions: ['.js', '.jsx'],
-    },
-    plugins: [
-      new webpack.optimize.UglifyJsPlugin(),
-      new HtmlWebpackPlugin({
-        template: __dirname + '/client/src/index.html',
-      }),
+          {
+            loader: 'css-loader',
+          },
+          {
+            loader: 'sass-loader',
+          },
+        ],
+      },
     ],
   },
-  {
-    entry: {
-      style: ['./client/src/sass/style.scss'],
-    },
-    output: {
-      path: path.resolve(__dirname, 'dist'),
-      filename: 'css/[name].css',
-    },
-    module: {
-      rules: [
-        {
-          test: /\.scss$/,
-          use: extractSass.extract({
-            use: [{
-              loader: 'css-loader',
-              options: {
-                sourceMap: true,
-              },
-            }, {
-              loader: 'sass-loader?outputStyle=compressed',
-            }],
-            fallback: 'style-loader',
-          }),
-        },
-      ],
-    },
-    plugins: [
-      extractSass,
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  optimization: {
+    minimizer: [
+      new OptimizeCSSAssetsPlugin(),
+      new UglifyJsPlugin(),
     ],
   },
-];
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, '/client/src/index.html'),
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'css/style.css',
+    }),
+  ],
+};
